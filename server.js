92a293bbb93f1ea31b773f0e8a7991854fee92f9
eXon <trembl.ben@gmail.com>
2015-07-30 10:00:04 -0400

    Removed unused file

--- server.js
+++
@@ -1,6 +0,0 @@
-ReactRouter = Npm.require('react-router');
-
-ReactRouter.lib = {
-  MemoryHistory: Npm.require('react-router/lib/MemoryHistory'),
-  Location: Npm.require('react-router/lib/Location')
-};
