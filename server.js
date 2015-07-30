5b38ad2e220820d9214489b776710d318250d176
eXon <trembl.ben@gmail.com>
2015-07-30 08:59:18 -0400

    Fixed location NPM path for server

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ ReactRouter = Npm.require('react-router');
 
 ReactRouter.lib = {
   MemoryHistory: Npm.require('react-router/lib/MemoryHistory'),
-  Location: Npm.require('react-router/lib/MemoryHistory')
+  Location: Npm.require('react-router/lib/Location')
 };
